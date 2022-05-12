e61581a1464b63ee7faf801d5b05ee4265cde87f
HermanLin <futurehlin@gmail.com>
2022-05-12 19:11:18 -0400

    new files for networking portion of the project

---
+++ Player.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * The Player class serves as the client portion of the
+ * Asteroids game. It connects to the Server in order to
+ * play with other players
+ *
+ * @author Herman Lin
+ * @author Devin Zhu
+ */
+
+public class Player {
+    
+}
---
+++ Server.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * This is the Server class that accepts connections from
+ * players. It also contains server-wide data that is necessary
+ * for gameplay.
+ *
+ * @author Herman Lin
+ * @author Devin Zhu
+ */
+
+public class Server {
+    
+}
