a9e8dd5043f576b375c4ec0ddda7bfefe874beb2
HermanLin <futurehlin@gmail.com>
2022-05-12 23:22:23 -0400

    added new constructor and docstrings

--- Data.java
+++ Data.java
@@ -21,6 +21,34 @@ class Data implements Serializable {
     // list of asteroids
     public ArrayList<Asteroid> asteroids;
 
+    /**
+     * This constructor is used there there is one or a 
+     * consistent number of players present on the Server. 
+     * Players only need to update each other on their Ships 
+     * as well as their Projectiles.
+     *
+     * @param player the object of the player sending data
+     * @param projectiles the object of the projectiles from the
+     *                    player sending data
+     */
+    public Data(Ship player, ArrayList<Projectile> projectiles) {
+        this.player = player;
+        this.projectiles = projectiles;
+    }
+
+    /**
+     * This constructor is used when there is a new Player
+     * joins a Server where there are already existing 
+     * Players. This is because the new Player needs to not 
+     * only know where all Players and their Projectiles are,
+     * but also the current Asteroid locations.
+     * 
+     * @param player the object of the player sending data
+     * @param projectiles the object of the projectiles from the
+     *                    player sending data
+     * @param asteroids the object of the current asteroids 
+     *                  present in the universe
+     */
     public Data(Ship player, 
                 ArrayList<Projectile> projectiles,
                 ArrayList<Asteroid> asteroids) {
